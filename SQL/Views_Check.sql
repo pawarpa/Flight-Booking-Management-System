29501e7f5eb577407ab6c3106faeceb7daff91b2
pawarpa <pawar.pa@northeastern.edu>
2024-01-04 20:27:32 -0500

    Created Directories

---
+++ SQL/Views_Check.sql
@@ -0,0 +1,5 @@
+USE P4_FBS
+GO
+SELECT * FROM Ticket
+SELECT * FROM Reservation_Status
+SELECT * FROM TravelClass_Services
--- Views_Check.sql
+++
@@ -1,5 +0,0 @@
-USE P4_FBS
-GO
-SELECT * FROM Ticket
-SELECT * FROM Reservation_Status
-SELECT * FROM TravelClass_Services
